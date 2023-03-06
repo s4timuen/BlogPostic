7cd4840606a44f564dc8a225c3796f75d9133f21
s4timuen <tim.muenzinger@yahoo.de>
2023-03-06 21:28:16 +0100

    made cookies accessable for web app

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -26,7 +26,7 @@ const createSendToken = (user, statusCode, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000,
         secure: process.env.NODE_ENV === 'production' ? true : false,
-        httpOnly: true,
+        httpOnly: false,
     }
 
     res.cookie(
